238d6bace55fc78ac9f8e7a8c2e612d0bf8c08ef
rndomhack <rndom.hack@gmail.com>
2015-08-03 01:40:00 +0900

    Update fsw.js

--- src/lib/fsw.js
+++ src/lib/fsw.js
@@ -9,19 +9,20 @@ class File {
     }
 
     exists() {
-        return new Promise((resolve, reject) => {
+        return new Promise(((resolve, reject) => {
             fs.stat(this._path, (err, stats) => {
                 if (err) {
                     reject(err);
                     return;
                 }
+
                 resolve();
             });
-        });
+        }).bind(this));
     }
 
     copy(dest) {
-        return new Promise((resolve, reject) => {
+        return new Promise(((resolve, reject) => {
             if (!(dest instanceof File))
                 throw new TypeError("Dest type is not File");
 
@@ -31,6 +32,68 @@ class File {
             write.on("error", err => reject(err));
             write.on("close", () => resolve());
             read.pipe(write);
-        });
+        }).bind(this));
+    }
+
+    move(dest) {
+        return new Promise(((resolve, reject) => {
+            if (!(dest instanceof File))
+                throw new TypeError("Dest type is not File");
+
+            fs.rename(this._path, dest, (err => {
+                if (err) {
+                    this.copy(dest).then(() => {
+                        return this.remove();
+                    }).then(() => {
+                        resolve();
+                    }).catch(err2 => {
+                        reject(err2);
+                    });
+                    return;
+                }
+
+                resolve();
+            }).bind(this));
+        }).bind(this));
+    }
+
+    remove() {
+        return new Promise(((resolve, reject) => {
+            fs.unlink(this._path, err => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve();
+            });
+        }).bind(this));
+    }
+
+    read(encode) {
+        return new Promise(((resolve, reject) => {
+            if (typeof encode !== "string")
+                throw new TypeError("Encode is not a string");
+
+            fs.readFile(this.path, {encoding: null}, (err, data) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                if (encode === null) {
+                    resolve(data);
+                } else {
+                    var converted;
+                    try {
+                        converted = encoding.convert(new Uint8Array(data), "UNICODE", encode, "string");
+                    } catch(err2) {
+                        reject(err2);
+                        return;
+                    }
+                    resolve(converted);
+                }
+            });
+        }).bind(this));
     }
 }
