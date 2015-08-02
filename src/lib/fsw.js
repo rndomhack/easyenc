5f46dd96a4437d1ef4b09ee841053588f8d87404
rndomhack <rndom.hack@gmail.com>
2015-08-03 02:23:27 +0900

    Add some methods to fsw.js

--- src/lib/fsw.js
+++ src/lib/fsw.js
@@ -96,4 +96,59 @@ class File {
             });
         }).bind(this));
     }
+
+    write(data, encode) {
+        return new Promise(((resolve, reject) => {
+            if (typeof encode !== "string")
+                throw new TypeError("Encode is not a string");
+
+            if (encode !== null) {
+                try {
+                    data = new Buffer(encoding.convert(data, encode, "UNICODE", "Array"));
+                } catch(err) {
+                    reject(err);
+                    return;
+                }
+            }
+            fs.writeFile(this.path, data, {encoding: null}, err => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve();
+            });
+        }).bind(this));
+    }
+
+    parent() {
+        return new Folder(path.dirname(this._path));
+    }
+
+    get path() {
+        return this._path;
+    }
+
+    get name() {
+        return path.basename(this._path);
+    }
+
+    get base() {
+        return path.basename(this._path, path.extname(this._path));
+    }
+
+    get ext() {
+        return path.extname(this._path);
+    }
 }
+
+class Folder {
+    constructor(arg) {
+        this._path = path.resolve(arg);
+    }
+}
+
+module.exports = {
+    File: File,
+    Folder: Folder
+};
