4d347a0f9105bafda4acf2e66ccf584f25c28586
rot1024 <rot1024@gmail.com>
2015-08-03 06:47:16 +0900

    Add cpw test

---
+++ test/lib/cpw_test.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var assert = require("assert");
+
+var Process = require("../../src/lib/cpw");
+
+describe("cpw", () => {
+
+    describe("Process", () => {
+
+        it("should get command", () => {
+            var process = new Process("node");
+            assert.strictEqual(process.command, "node");
+        });
+
+        it("should set command", () => {
+            var process = new Process("node");
+            process.command = "iojs";
+            assert.strictEqual(process.command, "iojs");
+        });
+
+        it("should get options", () => {
+            var process = new Process("node", { param: "test" });
+            var options = process.options;
+            assert.strictEqual(options.param, "test");
+        });
+
+        it("should set options", () => {
+            var process = new Process("node");
+            process.options = { param: "test" };
+            var options = process.options;
+            assert.strictEqual(options.param, "test");
+        });
+
+        it("should exec", done => {
+            var process = new Process("node -e process.exit(1)");
+            process.exec().then(err => {
+                assert.strictEqual(err.code, 1);
+                done();
+            }).catch(() => {
+                assert(false);
+                done();
+            });
+        });
+
+    });
+
+});
